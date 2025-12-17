383935ce6751dab54a8a5f805531aa72c37b5630
ritesh-jadhav <jadhav.ritesh0416@gmail.com>
2025-12-17 11:02:26 +0530

    average of every 3-element sliding window

---
+++ src/main/java/streams/jav/iq/l3/AvgOf3Element.java
@@ -0,0 +1,29 @@
+package streams.jav.iq.l3;
+
+// Given a list of integer, compute the average of every 3-element sliding window
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+// 1-2-3 = 6
+// 2-3-4 = 9
+// 3-4-5 = 12
+// 4-5-6 = 15
+
+public class AvgOf3Element {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(4,8,15,16,23,42);
+        List<Double> avg = IntStream.range(0, list.size() - 2)
+                .mapToObj(i -> Double.valueOf((double) (list.get(i) + list.get(i + 1) + list.get(i + 2)) / 3)).toList();
+
+        System.out.println(avg);
+
+
+    }
+
+
+}
